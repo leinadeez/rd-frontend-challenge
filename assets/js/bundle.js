af18bd9e52d043dae72b3034211771551459288f
leinadeez <leinadeez@gmail.com>
2020-01-31 19:33:40 -0300

    Teste resolvido

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,6 +1,6 @@
 (() => {
-    const selector = selector => /* trecho omitido */
-    const create = element => /* trecho omitido */
+    const selector = selector => document.querySelector(selector)
+    const create = element => document.createElement(element)
 
     const app = selector('#app');
 
@@ -13,17 +13,18 @@
 
     const Form = create('form');
 
-    Form.onsubmit = async e => {
-        e.preventDefault();
-        const [email, password] = /* trecho omitido */
+        Form.onsubmit = async e => {
+            e.preventDefault();
+            const [email, password] = [Form.email, Form.password]
 
-        const {url} = await fakeAuthenticate(email.value, password.value);
+            const {url} = await fakeAuthenticate(email.value, password.value);
+            console.log('teste', url)
 
-        location.href='#users';
-        
-        const users = await getDevelopersList(url);
-        renderPageUsers(users);
-    };
+            location.href='#users';
+            
+            const users = await getDevelopersList(url);
+            renderPageUsers(users);
+        };
 
     Form.oninput = e => {
         const [email, password, button] = e.target.parentElement.children;
@@ -32,53 +33,74 @@
             : button.removeAttribute('disabled');
     };
 
-    Form.innerHTML = /**
-    * bloco de código omitido
-    * monte o seu formulário
-    */
-
+    Form.innerHTML = `
+    <input type="email" required name="email" placeholder="Entre com seu e-mail" class="inputField" />
+    <input type="password" required name="password" placeholder="Digite sua senha supersecreta" class="inputField" />
+    <button type="submit" class="btnSubmit" disabled>Enviar</button>
+    `
+    
     app.appendChild(Logo);
     Login.appendChild(Form);
 
     async function fakeAuthenticate(email, password) {
-
-        /**
-         * bloco de código omitido
-         * aqui esperamos que você faça a requisição ao URL informado
-         */
-
-        const fakeJwtToken = `${btoa(email+password)}.${btoa(data.url)}.${(new Date()).getTime()+300000}`;
-        /* trecho omitido */
-
-        return data;
+        var data
+        try {
+            await fetch(
+                'http://www.mocky.io/v2/5dba690e3000008c00028eb6', 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                        'Authorization': 'Basic ' + `${btoa(email+password)}.${btoa('http://www.mocky.io/v2/5dba690e3000008c00028eb6')}.${(new Date()).getTime()+300000}`,
+                        'Accept': 'application/json'
+                    },
+                    body: `email=${email}&password=${password}&grant_type='password'`
+                }).then((response) => {
+                    data = response.json()
+                })
+            } catch (error) {
+                console.log(error)
+        }
+        return data
     }
 
     async function getDevelopersList(url) {
-        /**
-         * bloco de código omitido
-         * aqui esperamos que você faça a segunda requisição 
-         * para carregar a lista de desenvolvedores
-         */
+        var users
+        try {
+            await fetch(url, {method: 'GET'})
+            .then(response => {
+                users = response.json()
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        return users
     }
 
     function renderPageUsers(users) {
-        app.classList.add('logged');
-        Login.style.display = /* trecho omitido */
+        app.classList.add('logged')
+        Login.style.display = 'none'
 
-        const Ul = create('ul');
+        const Ul = create('ul')
         Ul.classList.add('container')
 
-        /**
-         * bloco de código omitido
-         * exiba a lista de desenvolvedores
-         */
-
+        users.forEach((user) => {
+            const Li = create('li')
+            Li.classList.add('dev')
+            Li.innerHTML = `
+            <a href='${user.html_url}' target='_blank'>
+            <img src='${user.avatar_url}' class='avatar' alt='Avatar de ${user.avatar_url}'>
+            <span>${user.login}</span>
+            </a>
+            `
+            Ul.appendChild(Li)
+        }) 
         app.appendChild(Ul)
     }
 
     // init
     (async function(){
-        const rawToken = /* trecho omitido */
+        const rawToken = null
         const token = rawToken ? rawToken.split('.') : null
         if (!token || token[2] < (new Date()).getTime()) {
             localStorage.removeItem('token');
